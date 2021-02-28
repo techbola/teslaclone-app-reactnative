c403a5d831d554924f88248cdf81b97a70e10c82
Adebola <bfagbowore@gmail.com>
2021-02-28 23:30:32 +0100

    Cart Item component

---
+++ components/CarItem/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import styles from "./styles";
+
+const CartItem = (props) => {
+  return (
+    <View style={styles.carContainer}>
+
+      <ImageBackground
+        source={require('../../assets/images/ModelS.jpeg')}
+        style={styles.image}
+      />
+
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69, 000</Text>
+      </View>
+
+    </View>
+  );
+};
+
+export default CartItem;
